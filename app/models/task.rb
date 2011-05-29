cc19784b84bb98b6e16cfed5173b5e73cc789dde
Tyler Moore <tylerlmoore@gmail.com>
2011-05-29 12:01:58 -0400

    first commit

---
+++ app/models/task.rb
@@ -0,0 +1,6 @@
+class Task < ActiveRecord::Base
+  validates_presence_of :name
+  validates_length_of :name, :maximum=>30, :too_long => "must be less 30 characters."
+  validates_length_of :description, :maximum=>100, :too_long => "must be less than 100 characters."
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToDo::Application.config.secret_token = 'dad759b456eb69149b69d4ed07ef0b92ceda3338d54015020dbf55d1ccbbda0f8c4709fd15d0cb20a19becd5f541da5495764c342181849b7c654b78f87128bc'
